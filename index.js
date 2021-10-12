a7ca8185eeab6aa680588fd97ee0304c3508a1ab
Kim Hyunjae <loma1016@gmail.com>
2021-10-12 21:54:19 +0900

    commands 추가

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
+// javascript file system module
+const fs = require('fs');
+
+// client 인스턴스 생성
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
+client.commands = new Collection();
+
+// commands 폴더에 있는 파일 중 .js로 끝나는 파일들을 필터링하여 새로운 배열 생성
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+// 클라이언트가 준비되었을 때 한 번 실행
 client.once('ready', () => {
   console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
 });
 
 // Login to Discord with your client's token
