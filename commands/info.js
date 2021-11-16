162cfc771046ecfb645f62764f942e2c4ab2c965
Kim Hyunjae <loma1016@gmail.com>
2021-11-16 14:42:14 +0900

    크롤링 후 정보 가공

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,9 @@
-const { CommandInteraction, MessageEmbed } = require('discord.js');
+const { CommandInteraction, MessageEmbed, Message } = require('discord.js');
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+// 칭호, 길드, pvp, 영지
+let gameInfo = [];
 
 module.exports = {
   name: 'info',
@@ -10,10 +13,22 @@ module.exports = {
    */
 
   async execute(interaction) {
-    const gameInfo = getGameInfo();
-    console.log(gameInfo);
+    try {
+      // 칭호, 길드, pvp, 영지 데이터 가져오기
+      await getGameInfo();
 
-    await interaction.reply({ content: 'ii', ephemeral: true });
+      // 임베드 만들어주기
+      const infoEmbed = new MessageEmbed().setColor('#0099ff').setTitle('해당 위치에 닉네임 가져올 예정').addFields(
+        // map으로 변경예정
+        { name: gameInfo[0].title, value: gameInfo[0].des },
+        { name: gameInfo[1].title, value: gameInfo[1].des },
+        { name: gameInfo[2].title, value: gameInfo[2].des },
+        { name: gameInfo[3].title, value: gameInfo[3].des }
+      );
+      await interaction.reply({ embeds: [infoEmbed], ephemeral: false });
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
@@ -27,20 +42,19 @@ const getHtml = async () => {
   }
 };
 
-const getGameInfo = () => {
-  getHtml().then((html) => {
+const getGameInfo = async () => {
+  await getHtml().then((html) => {
     const $ = cheerio.load(html.data);
     const gameInfoList = $('div.game-info').children('div');
-    let gameInfoData = [];
+
     gameInfoList.each(function (i, element) {
-      gameInfoList[i] = {
-        title: $(this).find('span').first().text(),
-        des: $(this).find('span').last().text()
-      };
-      gameInfoData[i] = gameInfoList[i].title;
+      // gameInfoList[i] = {
+      //   title: $(this).find('span').first().text(),
+      //   des: $(this).find('span').last().text()
+      // };
+      gameInfo[i] = { title: $(this).find('span').first().text(), des: $(this).find('span').last().text() };
     });
-    // embed 생성
-    console.log(gameInfoList);
-    return gameInfoList;
+    console.log(gameInfo);
   });
+  return gameInfo;
 };
